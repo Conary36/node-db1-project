34a0927d8338f2d4abbe2682bda4bc848cf22542
Conary36 <cbeckford144@gmail.com>
2020-04-13 12:59:45 -0400

    updated #20 Stretch goal

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-SELECT * FROM [Customers] WHERE PostalCode = 1010 LIMIT 3
+SELECT * FROM [Customers] WHERE PostalCode = 1010
 -- Find the phone number for the supplier with the id 11
 SELECT * FROM [Suppliers] 
 WHERE SupplierID = 11
@@ -17,7 +17,7 @@ UPDATE Customers
 Set PostalCode = '11122'
 WHERE CustomerName = 'Bilbo Baggins'
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-SELECT DISTINCT Country FROM Customers;
+SELECT COUNT (DISTINCT City) FROM Customers; -- add COUNT method 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
 SELECT * FROM [Suppliers]
 WHERE LENGTH(SupplierName) > 20
